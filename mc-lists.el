c756f752c2b514f52c19c0517ea96015bf58689e
gahag <gahag@hotmail.com.br>
2016-09-29 14:34:31 -0300

    Updated mc-lists.el

--- mc-lists.el
+++ mc-lists.el
@@ -3,20 +3,33 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        backward-kill-sentence
         backward-sexp
         c-electric-brace
+        c-electric-colon
         c-electric-delete-forward
+        c-electric-lt-gt
         c-electric-paren
         c-electric-semi&comma
         c-electric-slash
+        c-electric-star
+        c-end-of-statement
         comment-dwim
+        csharp-maybe-insert-codedoc
         cycle-spacing
         dabbrev-expand
         delete-horizontal-space
+        electric-newline-and-maybe-indent
         electric-pair-delete-pair
         forward-sexp
-        helm-confirm-and-exit-minibuffer
+        haskell-indentation-delete-backward-char
+        haskell-indentation-delete-char
+        helm-mini
         kill-region
+        kill-sentence
+        org-delete-char
+        org-self-insert-command
+        org-todo
         tab-to-tab-stop
         transpose-sexps
         transpose-words
@@ -27,8 +40,11 @@
 (setq mc/cmds-to-run-once
       '(
         2C-two-columns
-        c-electric-colon
+        ace-jump-mode
+        delete-rectangle
+        helm-confirm-and-exit-minibuffer
         helm-find-files
+        helm-show-kill-ring
         kmacro-end-or-call-macro
         mc/mark-next-lines
         mc/mark-previous-lines
