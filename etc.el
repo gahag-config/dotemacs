bfc9099fd406748b6286b8f984406143b670bca3
gahag <gahag@hotmail.com.br>
2016-10-26 21:19:31 -0200

    Fixed haskell-mode and org-mode bindings

--- etc.el
+++ etc.el
@@ -8,10 +8,12 @@
 (eval-after-load 'haskell-font-lock
  '(progn (defconst haskell-font-lock-symbols-alist
                    '(("\\" . "λ") ("." "∘" haskell-font-lock-dot-is-not-composition)))
-         (setq haskell-font-lock-keywords (haskell-font-lock-keywords-create nil))))
+         ;(setq haskell-font-lock-keywords (haskell-font-lock-keywords-create nil))
+         ))
 
 
 ;; These conflict with windmove.
+(require 'org)
 (define-key org-mode-map (kbd "<M-up>")    nil)
 (define-key org-mode-map (kbd "<M-down>")  nil)
 (define-key org-mode-map (kbd "<M-left>")  nil)
