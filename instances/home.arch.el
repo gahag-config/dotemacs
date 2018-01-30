ee6dc697eaa5ab7b2a42586fcd24f0596b74b094
gahag <gabriel.s.b@live.com>
2018-01-30 04:27:09 -0200

    Added source code pro font to home.arch instance.

--- instances/home.arch.el
+++ instances/home.arch.el
@@ -22,7 +22,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight normal :height 98 :width normal)))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
