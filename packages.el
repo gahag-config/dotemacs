7ca13cf4667e7ec2aefddce1a00b9c6d5ed8b174
gahag <gabriel.s.b@live.com>
2018-06-11 03:24:11 -0300

    Added intero to packages list.

--- packages.el
+++ packages.el
@@ -7,6 +7,7 @@
                          color-theme
                          csharp-mode
                          haskell-mode
+                         intero
                          elpy
                          expand-region
                          flycheck
