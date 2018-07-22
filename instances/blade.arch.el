120ae35aa2ce173a6ec69dd78c27f75f41f16042
gahag <gabriel.s.b@live.com>
2018-07-21 21:43:41 -0300

    Updated blade instance. Updated mc-lists.

--- instances/blade.arch.el
+++ instances/blade.arch.el
@@ -21,7 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight normal :height 97 :width normal)))))
+ '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight normal :height 100 :width normal)))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
