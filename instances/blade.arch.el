91abd0f5c29735e8675dc11275fd7fd0632f7782
gahag <gabriel.s.b@live.com>
2018-08-04 19:57:52 -0300

    Updated instance.

--- instances/blade.arch.el
+++ instances/blade.arch.el
@@ -12,17 +12,12 @@
 (add-to-list 'custom-theme-load-path themes-dir)
 (load-theme 'doom-gahag t)
 (powerline-gahag-theme)
-
-
-;; ---------------------------------------------------------------------------------------
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight normal :height 100 :width normal)))))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -33,4 +28,4 @@
     ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
  '(package-selected-packages
    (quote
-    (dired-quick-sort try htmlize ledger-mode calfw calfw-org company doom-themes eglot shm transpose-frame use-package yasnippet-snippets dired-ranger latex-preview-pane elpy magit intero nyan-mode ace-jump-mode flymake-haskell-multi haskell-mode pdf-tools markdown-mode helm multiple-cursors expand-region yasnippet flycheck powerline auto-complete))))
+    (projectile company-lsp cquery lsp-mode lsp-python lsp-ui dired-quick-sort try htmlize ledger-mode calfw calfw-org company doom-themes shm transpose-frame use-package yasnippet-snippets dired-ranger latex-preview-pane elpy magit intero nyan-mode ace-jump-mode flymake-haskell-multi haskell-mode pdf-tools markdown-mode helm multiple-cursors expand-region yasnippet flycheck powerline auto-complete))))
