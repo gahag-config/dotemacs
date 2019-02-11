fed1e3f5fe12df65bd6b2cb6f08f9e1d1b10999b
gahag <gabriel.s.b@live.com>
2019-02-11 14:20:46 -0200

    Rebing <C-i> to yank in GUI mode.

--- keys.el
+++ keys.el
@@ -1,5 +1,6 @@
 (global-unset-key "") ;; ESC ESC ESC
 
+
 ;; (global-set-key (kbd "C-x k") 'kill-this-buffer)
 (global-set-key (kbd "C-x k") 'context-kill-buffer)
 (global-set-key (kbd "C-;"  ) 'comment-line)
@@ -12,3 +13,11 @@
 (global-set-key (kbd "s-a") 'windmove-left)
 
 (global-set-key (kbd "C-x t") 'term)
+
+
+;; GUI only keybindings:
+(if (window-system)
+    (progn
+      ;; Remap C-i to yank:
+      (define-key input-decode-map [?\C-i] [C-i])
+      (global-set-key (kbd "<C-i>") 'yank)))    ; Angled brackets required due to decoding.
