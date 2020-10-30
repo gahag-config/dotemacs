64c3c75a6dbe8508dd1d0ecc321d787500f110d6
gahag <gabriel.s.b@live.com>
2020-10-29 21:03:19 -0300

    Update lsp-ui sideline face

--- themes/doom-gahag-light-theme.el
+++ themes/doom-gahag-light-theme.el
@@ -254,7 +254,7 @@ determine the exact padding."
    (lsp-face-highlight-textual :weight 'bold)
    (lsp-face-highlight-read :weight 'bold)
    (lsp-face-highlight-write :weight 'bold)
-   (lsp-ui-sideline-code-action :foreground orange)
+   (lsp-ui-sideline-code-action :foreground (doom-darken grey 0.3))
    (lsp-ui-sideline-symbol :foreground base7)
    (lsp-ui-sideline-current-symbol :foreground fg :weight 'medium)
    (lsp-ui-sideline-symbol-info :foreground base7)
