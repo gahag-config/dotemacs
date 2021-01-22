0d86895e260537b37b66a31203c4e8e6aedfc3f7
gahag <gabriel.s.b@live.com>
2021-01-22 14:11:06 -0300

    Update motherbase instance

--- instances/motherbase.arch.el
+++ instances/motherbase.arch.el
@@ -21,7 +21,11 @@
                              feature-python
                              feature-rust
                              feature-ledger
-                             feature-org-presentation))
+                             feature-org-presentation
+                             feature-org-calc
+                             feature-docker
+                             feature-javascript
+                             feature-restclient))
 
 
 (load dotemacs-file)
@@ -43,5 +47,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(package-selected-packages
-   '(org-tree-slide olivetti htmlize vterm sml-mode multi-line yasnippet-snippets yasnippet pdf-tools rust-mode web-mode docker org-bullets lsp-mode diminish lsp-ui magit multi-term rainbow-mode ccls dired-narrow dired-subtree use-package lsp-java rg dockerfile-mode json-mode lua-mode use-package-ensure-system-package highlight-indent-guides wakatime-mode diff-hl company-lsp cquery lsp-python org togetherly dired-quick-sort dired-ranger ledger-mode try csharp-mode flycheck markdown-mode transpose-frame calfw-org calfw csv-mode rainbow-delimiters gruvbox-theme doom-themes flymd flycheck-haskell yaml-mode impatient-mode latex-preview-pane intero lorem-ipsum gh-md markdown-preview-mode powerline omnisharp multiple-cursors heroku-theme helm haskell-mode fsharp-mode flatui-theme expand-region elm-mode company ace-jump-mode)))
+   '(js2-mode restclient org-ref literate-calc-mode org-tree-slide olivetti htmlize vterm sml-mode multi-line yasnippet-snippets yasnippet pdf-tools rust-mode web-mode docker org-bullets lsp-mode diminish lsp-ui magit multi-term rainbow-mode ccls dired-narrow dired-subtree use-package lsp-java rg dockerfile-mode json-mode lua-mode use-package-ensure-system-package highlight-indent-guides wakatime-mode diff-hl company-lsp cquery lsp-python org togetherly dired-quick-sort dired-ranger ledger-mode try csharp-mode flycheck markdown-mode transpose-frame calfw-org calfw csv-mode rainbow-delimiters gruvbox-theme doom-themes flymd flycheck-haskell yaml-mode impatient-mode latex-preview-pane intero lorem-ipsum gh-md markdown-preview-mode powerline omnisharp multiple-cursors heroku-theme helm haskell-mode fsharp-mode flatui-theme expand-region elm-mode company ace-jump-mode)))
